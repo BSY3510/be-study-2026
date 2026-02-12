2d0f003f14d6a160238a29217e3e3e49468aba55
BSY3510 <baeksy3510@gmail.com>
2026-02-13 00:53:46 +0900

    feat: 5주차 코드 복사

---
+++ src/main/java/com/example/be_study_2026/week6/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.example.be_study_2026.week6;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    Optional<Menu> findByName(String name);
+}
---
+++ src/main/java/com/example/be_study_2026/week6/Week6Application.java
@@ -0,0 +1,12 @@
+package com.example.be_study_2026.week6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Week6Application.class, args);
+    }
+
+}
